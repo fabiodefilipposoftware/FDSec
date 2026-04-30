b0cf9e340cab43539d073d4263f65ffda13dd37e
FabioDefilippo <fabiodefilipposoftware@gmail.com>
2026-04-30 13:16:15 +0200

    Fixed last bugs

--- script.py
+++ script.py
@@ -9,18 +9,18 @@ def main():
     if not string_to_search:
         return false
     
-    if os.path.exists(file_path):
+    if os.path.exists(file_database):
         with open(file_path, 'r', encoding='utf-8') as f:
             database_set = {line.strip() for line in f if line.strip()}
   
     found = False
     
-    if search_target in database_set:
+    if string_to_search in database_set:
         found = True
 
     result_text = "MALWARE" if found else "good"
     requests.post("https://github.com/fabiodefilipposoftware/fdsec.it", json={
-        "string": search_target,
+        "string": string_to_search,
         "verdict": result_text
     })
 if __name__ == "__main__":
