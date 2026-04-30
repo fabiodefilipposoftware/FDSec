4fd0863d7c64286e7778d40005de89130cce9ca2
FabioDefilippo <fabiodefilipposoftware@gmail.com>
2026-04-30 12:13:21 +0200

    Update script.py

--- script.py
+++ script.py
@@ -5,25 +5,25 @@ def main():
     string_to_search = os.getenv('key1')
     file_database = "Database/malwaresignatures.txt"
     
-    if not stringa_da_cercare:
+    if not string_to_search:
         # print("ERROR: no string riceived...")
         return
 
     # print(f"Searching {string_to_search} ...")
-    trovato = False
+    found = False
 
     if os.path.exists(file_database):
         with open(file_database, 'r', encoding='utf-8') as f:
-            for riga in f:
-                if string_to_search.strip() in riga:
-                    trovato = True
+            for line in f:
+                if string_to_search.strip() in line:
+                    found = True
                     break
     else:
         # print(f"The file {file_database} does not exist.")
 
-    result_text = "MALWARE" if trovato else "good"
+    result_text = "MALWARE" if found else "good"
     requests.post("https://github.com/fabiodefilipposoftware/fdsec.it", json={
-        "stringa": search_target,
+        "string": search_target,
         "verdict": result_text
     })
 if __name__ == "__main__":
